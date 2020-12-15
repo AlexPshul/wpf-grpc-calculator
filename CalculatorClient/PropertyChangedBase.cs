e65a2f04d3eeac8a5f76c63c1e9e01b58eea1bf6
Alex Pshul <alex@pshul.com>
2020-12-15 19:53:51 +0200

    Added basic calculator layout

---
+++ CalculatorClient/PropertyChangedBase.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using CalculatorClient.Annotations;
+
+namespace CalculatorClient
+{
+    public class PropertyChangedBase : INotifyPropertyChanged
+    {
+        protected void Set<T>(ref T property, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (property?.Equals(value) ?? value == null)
+                return;
+
+            property = value;
+            OnPropertyChanged(propertyName);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
